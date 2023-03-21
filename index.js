9bf2c8975d96ccf0d8af547bdd82094a4600b4e5
rodqui <110335551+rodqui@users.noreply.github.com>
2023-03-21 22:19:34 +0100

    update

--- index.js
+++ index.js
@@ -1,56 +1,63 @@
 
 
 var lenght=document.querySelectorAll("button").length;
-var boton = document.querySelector(".botonprueba");
+
 var i= 0;
 while(i<lenght){
-    document.querySelectorAll("button")[i].addEventListener("oncanplaythrough", 
+    document.querySelectorAll("button")[i].addEventListener("click", 
     function (){ //anonymous function
         
        var letter = this.innerHTML;
-       switch(letter){
-            case "w":
-                var sound = new Audio("sounds/snare.mp3");
-                sound.play();
-            case "a":
-                var sound = new Audio("sounds/tom-1.mp3");
-                sound.play();
-            break;
-            case "s":
-                var sound = new Audio("sounds/tom-2.mp3");
-                sound.play();
-            break;
-            case "d":
-                var sound = new Audio("sounds/tom-3.mp3");
-                sound.play();
-            break;
-            case "j":
-                var sound = new Audio("sounds/crash.mp3");
-                sound.play();
-            break;
-            case "k":
-                var sound = new Audio("sounds/kick-bass.mp3");
-                sound.play();
-            break;
-            case "l":
-                var sound = new Audio("sounds/tom-4.mp3");
-                sound.play();
-            break;
-
-            default:
-                console.log("press a correct button");
-       }
+       soundSelected(letter);
        
     });
     i++;
 }
 
 
-boton.addEventListener("click", function(){
-    var sound2 = new Audio("sounds/tom-4.mp3");
-    sound2.oncanplaythrough = function(){
-        sound2.play();
-    };
-   
-    
+
+document.addEventListener("keydown", function(event){
+    console.log(event.key);
+
+    soundSelected(event.key);
+
 });
+
+
+
+
+function soundSelected(key){
+    switch(key){
+        case "w":
+            var sound1 = new Audio("sounds/snare.mp3");
+            sound1.play();
+        case "a":
+            var sound2 = new Audio("sounds/tom-1.mp3");
+            sound2.play();
+        break;
+        case "s":
+            var sound3 = new Audio("sounds/tom-2.mp3");
+            sound3.play();
+        break;
+        case "d":
+            var sound4 = new Audio("sounds/tom-3.mp3");
+            sound4.play();
+        break;
+        case "j":
+            var sound5 = new Audio("sounds/crash.mp3");
+            sound5.play();
+        break;
+        case "k":
+            var sound6 = new Audio("sounds/kick-bass.mp3");
+            sound6.play();
+        break;
+        case "l":
+            var sound7 = new Audio("sounds/tom-4.mp3");
+            sound7.play();
+        break;
+
+        default:
+            console.log("press a correct button");
+   }
+
+}
